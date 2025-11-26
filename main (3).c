5cbbade979016948221969803dc97cb47d0c52bf
Kendrick Halder <108589219+halderkendrick@users.noreply.github.com>
2025-11-26 10:28:15 +0600

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int count =1;
+    scanf("%d", &num);
+    for(int i=1; i<=num; i++){
+        for(int j=1; j<=i; j++){
+            printf("%d ", count);
+            count++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
